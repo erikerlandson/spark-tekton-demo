9094e7e615302d94187b22c185a44988a69d5de8
Erik Erlandson <eerlands@redhat.com>
2021-01-15 10:34:05 -0700

    spark.executor.memory

--- examples/spark-pi/spark-app.py
+++ examples/spark-pi/spark-app.py
@@ -10,12 +10,14 @@ from pyspark.sql import SparkSession
 spark_conf = SparkConf()
 
 # Configure some basic spark cluster sizing parameters
-spark_conf.set('spark.cores.max', 2)
 spark_conf.set('spark.executor.cores', '1')
+spark_conf.set('spark.executor.instances', '2')
+spark_conf.set('spark.executor.memory', '900m')
 
 # The name of your Spark cluster hostname or ip address
 spark_cluster = os.environ['SPARK_CLUSTER']
 
+print('Attaching to Spark cluster {cluster}'.format(cluster=spark_cluster)) 
 spark = SparkSession.builder \
     .master('spark://{cluster}:7077'.format(cluster=spark_cluster)) \
     .appName('Spark-App-S2I-Demo') \
@@ -30,7 +32,11 @@ def f(_):
     y = random() * 2 - 1
     return 1 if x ** 2 + y ** 2 <= 1 else 0
 
+print('Running Spark job to compute pi')
 count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+
+print('====================================================')
 print("Pi is roughly %f" % (4.0 * count / n))
+print('====================================================')
 
 spark.stop()
